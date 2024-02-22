be760c12fe88d3bac5c0bd5aa5957e1e044bacd7
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-02-22 21:54:54 +0500

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,10 @@
+// we have to define function by this method
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(20, 90));
+// we define type by using:
+function mul(a, b) {
+    return a * b;
+}
+console.log(mul(20, 90));
---
+++ class1.ts
@@ -0,0 +1,13 @@
+// we have to define function by this method
+
+function sum(a,b){
+    return a+b
+}
+console.log(sum(20,90))
+
+// we define type by using:
+
+function mul(a:number,b:number):number{
+    return a*b
+}
+console.log(mul(20,90))
